7822794d208611c649d810ae7aaa89fce6009996
boouee <104202645+boouee@users.noreply.github.com>
2024-01-16 19:13:08 +0300

    Update upload.tsx

--- src/components/upload.tsx
+++ src/components/upload.tsx
@@ -13,7 +13,9 @@ class Upload extends Component {
 	// On file select (from the pop up)
 	onFileChange = (event: Event) => {
 		// Update the state
-		if (event.target.files) {
+		if (!event.target.files) {
+			return;
+		} else {
 			this.setState({
 				selectedFile: event.target.files[0],
 			});
