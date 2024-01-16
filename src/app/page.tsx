710b8788dd187d1592dda93196e1018d0c9470d4
boouee <104202645+boouee@users.noreply.github.com>
2024-01-16 18:05:07 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ axios.get(XMLData, {
   //const res = xml2js(xml)
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
- */
+ 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data')
